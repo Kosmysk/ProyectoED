f0e769cbbce6812cd2b1e8ce07bf63c7420f3c23
Tomás Cisternas Araya <122064986+DeltaNiz@users.noreply.github.com>
2024-06-18 18:13:07 -0400

    Update main.cpp
    eliminada la funcion para guardar archivos en general (ya no servia), metodos de guardar y leer archivos de cada header movidos a su respectivo header, añadida nueva estampa de tiempo entre los experimentos, ordenado el main para probar todos los metodos. todo funcional

--- main.cpp
+++ main.cpp
@@ -24,114 +24,84 @@ string leerArchivo(const string& filename) {
     return texto;                //retorna el texto del archivo
 }
 
-// Funcion para leer el archivo .bin que se genera con saveBinaryFile
-string leerArchivoBin(const string &filename){
-    ifstream file(filename, ios::binary); // se abre el archivo con ifstream en modo binario
-    // En esta parte se declara el vector de lo bytes del archivo y se guardan estos bytes en el vector
-    vector<unsigned char> byteVector((istreambuf_iterator<char>(file)), istreambuf_iterator<char>()); //estos istreambuf_iterator sirven para iterar sobre el archivo, el primero
-    // itera sobre todos los char en file, que en este caso son los bytes del archivo, el que está vacio es para finalizar esta especie de range 
-
-    string bitString; //se declara un bitString que contiene los bits del archivo pero como un string
-    for (unsigned char byte : byteVector){ // El ciclo for trabaja sobre cada byte, que convenientemente es un unsigned char ya que solo almacena 1 byte, en byteVector
-        bitString += bitset<8>(byte).to_string(); //usando bitset podemos representar de maneria binaria este byte en 8 bits, y con to_string queda como string.
-    }
-
-    return bitString; //Devuelve este string que contiene los bits
+// Funcion para guardar los resultados
+void logResults(ofstream &output, double total_time, double codecomp_time, double decodecomp_time){
+    output << total_time << ";" << codecomp_time << ";" << decodecomp_time << endl;
 }
 
-// Funcion para transformas los bits de string que devuelve nuestro huffman a un archivo .bin como corresponde
-void saveBinaryFile(const string &bitString, const string binaryfile){
-    // abrimos el archivo donde vamos a escribir el string como bin, esto se hace con std::ios::binary, asi nos aseguramos que el resultado sea binario si o si
-    ofstream file(binaryfile, ios::binary);
-
-    vector<unsigned char> byteVector; //Como no podemos escribir bit por bit, hacemos un vector de bytes, que guarda los bits del bitstring
-    int bitcount = 0; //Se utiliza bitcount para contar los bits que entran al vector
-    unsigned char byte = 0; // Se inicializa un byte para acumular los bits con unsigned char, lo que es perfecto porque solo almacena 8 bits
-
-    for(char bit : bitString){ // En este for se itera sobre cada bit en el bitstring
-        if (bit == '1'){ // Sabemos que en un byte hay 8 bits, y que estos van de 0 a 7, en esta parte movemos el bit de valor 1 hacia la izquierda, es decir 10000000
-            byte |= (1<<(7 - bitcount)); //  Ahora que tenemos el bit como 10000000 con el operador OR este se añade al byte y nos quedaría el byte como 10000000
-        }     
-        bitcount ++; //Suma 1 a bitcount, si el bit es 0 suma uno directamente, es decir que si el string de bits es '101' el byte nos quedaria como 10100000, ya que para 
-        // el tercer bit, el bit '1' la bitcount seria 2 y esto dejaria 7-2 dejandolo en la quinta posicion
-
-        if (bitcount == 8){ // Como los bytes solo almacenan 8 bits, cuando esta cuenta llega a 8, este byte completo se añade al byteVector
-            byteVector.push_back(byte);
-            bitcount = 0; // Y se reinician el bitcount y el byte para ir contando bits de nuevo e ir agregando bytes al bytevector
-            byte = 0;
-        }
-    }
-
-    if(bitcount > 0){ // si es que sobran bits que no completaron un byte, esto se encarga de meterlo al vector
-        byteVector.push_back(byte);
-    }
-
-    // aca se escriben los resultados en el archivo, write espera un const char asi que se usa reintrepret_cast para transformar la data de byteVector en un const char
-    file.write(reinterpret_cast<const char *>(byteVector.data()), byteVector.size()); //byteVector.size() es el tamaño del vector array, es decir los bytes que se deben escribir.
-    file.close(); //Se cierra el archivo
-}
-
-// Funcion para guardar los archivos, para hacer pruebas de codificacion/compresion y decodificacion/decompresion por separado
-void saveFile(ofstream  &output, string &codified){
-    output << codified;
-}
-
-// Funcion para guardar los resultados, no sé que más agregarle, solo muestra el tiempo del 1 al 20 como una columna, si alguno tiene alguna idea modifiquelo pls sdjds.
-void logResults(ofstream &output, double time){
-    output << time << endl;
-}
 
 // Funcion que mide el tiempo que toma en codificar y decodificar 20 veces
 void TimeExperiment(const string &filename, const string &resultadosCodificacion, const string &resultadosCompresion){
-    HuffmanCoding huffman;
-    ofstream file(resultadosCodificacion);
-    ofstream file2(resultadosCompresion);
-    string original = leerArchivo(filename);
+    HuffmanCoding huffman; // Llamamos la class huffman para traer sus funciones
+    ofstream file(resultadosCodificacion); //Abrimos el archivo donde guardaremos los tiempos de codificacion
+    ofstream file2(resultadosCompresion); // Abrimos el archivo donde guardaremos los tiempos de compresion
+    string original = leerArchivo(filename); // Abrimos el archivo que contiene los datos originales
 
-    for (int i = 0; i < 20; ++i){
+    for (int i = 0; i < 20; ++i){ // En este ciclo for se especifica que se hace el experimento 20 veces, de 0 a 19
         
-        auto start = chrono::high_resolution_clock::now();
-        string codified = huffman.encode(original);
-        string decodified = huffman.decode(codified);
-        auto end = chrono::high_resolution_clock::now();
-        chrono::duration<double> execution_time = end - start;
-        logResults(file, execution_time.count());
+        auto start = chrono::high_resolution_clock::now(); //Tomamos el tiempo al principio del experimento
+        string codified = huffman.encode(original); //codificacion
+        auto tstep = chrono::high_resolution_clock::now(); //Tomamos el tiempo justo despues de la codificacion y antes de la decodificacion
+        string decodified = huffman.decode(codified); // decodificacion
+        auto end = chrono::high_resolution_clock::now(); // Tomamos el tiempo justo despues de decodificar
+        chrono::duration<double> totaltime = end - start; // Tiempo total de codificar y decodificar
+        chrono::duration<double> codetime = tstep - start; // tiempo que solo se demora en codificar
+        chrono::duration<double> decodetime = end - tstep; // tiempo que se demora solo en decodificar
+        logResults(file, totaltime.count(), codetime.count(), decodetime.count()); // guardamos los resultados
 
-        start = chrono::high_resolution_clock::now();
+        start = chrono::high_resolution_clock::now(); // Exactamente lo mismo que el anterior, pero para la compresion y decompresion
         vector<pair<int, int>> comprimido = compresionLZ(original);
+        tstep = chrono::high_resolution_clock::now();
         string descomprimido = descompresionLZ(comprimido);
         end = chrono::high_resolution_clock::now();
-        execution_time = end - start;
-        logResults(file2, execution_time.count());  
-
+        totaltime = end - start;
+        chrono::duration<double> comptime = tstep - start;
+        chrono::duration<double> decomptime = end - tstep;
+        logResults(file2, totaltime.count(), comptime.count(), decomptime.count());  
         
     }
-    file.close();
+    file.close(); // Cerramos ambos archivos
     file2.close();
 }
 
+
 int main(){
-    HuffmanCoding huffman;
-
-    string dataprueba = "prueba.txt";
-    string textobin = "textoprueba.bin";
-    //TimeExperiment(dataprueba, "resultadoshuffman.csv", "resultadosLZ.csv");
-
-    string original = leerArchivo(dataprueba);
+    //Cargamos los datos
+    string data = leerArchivo("prueba.txt");
     
-    string codified = huffman.encode(original);
-    string bitstring = leerArchivoBin(textobin);
+    // HUFFMAN ENCODING
+    HuffmanCoding huffman;
+    string codified = huffman.encode(data);
+    huffman.saveEncoded(codified, "pruebacodified.bin");
+    string codifiedbin = huffman.loadEncoded("pruebacodified.bin");
+    string decodified = huffman.decode(codifiedbin);
 
-    string decodificado = huffman.decode(bitstring);
-    cout << decodificado;
-
-    // Para probar si el texto original es igual al decodificado, considerando que la decodificacion se hizo desde el archivo .bin
-    if(original == decodificado){
-        cout << "TA BUENO" << endl;
+    if(decodified == data){
+        cout << "Decodificacion correcta." << endl;
     }
     else{
-        cout << "TA MALO" << endl; 
+        cout << "Decodificacion incorrecta." << endl;
     }
 
+    // LZ COMPRESSION
+    vector<pair<int, int>> compressed = compresionLZ(data);
+    saveComp(compressed, "pruebacompressed.bin");
+    vector<pair<int, int>> compressedbin = loadComp("pruebacompressed.bin");
+    string decompressed = descompresionLZ(compressedbin);
 
+    if(decompressed == data){
+        cout << "Descompresion correcta." << endl;
+    }
+    else{
+        cout << "Descompresion incorrecta." << endl;
+    }
+    
+    // Experimentos para medir tiempo, si solo se quiere medir el tiempo se puede borrar el resto del main.
+    string txtexperimento = "prueba.txt"; // como en TimeExperiment() esta implementado el LeerArchivo() tenemos que ingresar filename como un string
+    TimeExperiment(txtexperimento, "Resultados_Huffman.csv", "Resultados_LZ.csv");
+
+    return 0;
 }
+
+// En la funcion main se prueban los metodos de los header Huffman.h y LZ.h, luego se compara el resultado final
+// con el texto original para ver si funciona correctamente.
